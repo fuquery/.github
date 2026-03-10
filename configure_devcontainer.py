7c2251985bebc48fcb494ab9871d99254930aea4
Tsche <che@pydong.org>
2026-03-10 03:19:39 +0100

    configure devcontainer hook

---
+++ configure_devcontainer.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import shutil
+
+MANIFEST_PATH = "manifest"
+
+def main(repo_topdir=None, **kwargs):
+    """Main function invoked directly by repo.
+
+    We must use the name "main" as that is what repo requires.
+
+    Args:
+      repo_topdir: The absolute path to the top-level directory of the repo workspace.
+      kwargs: Leave this here for forward-compatibility.
+    """
+    repo = Path(repo_topdir)
+    devcontainer_path = repo / ".devcontainer"
+    devcontainer_path.mkdir(exist_ok=True)
+    
+    configuration = repo / MANIFEST_PATH / "devcontainer.json"
+    if configuration.exists():
+        shutil.copy(configuration, devcontainer_path / "devcontainer.json")
+    else:
+        print(f"Warning: No devcontainer.json found at {configuration}. Skipping devcontainer configuration.")
+
+if __name__ == "__main__":
+    main()
